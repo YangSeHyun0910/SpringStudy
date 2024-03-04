ce2942419ad5974382482d88bd8fb2f915c026bf
Yangsehyun0910 <yangsehyun1@gmail.com>
2024-03-04 18:05:18 +0900

    회원가입 폼 처리까지

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,5 +16,22 @@ public class MemberController {
     //@Autowired 를 사용하면, 스프링이 스프링컨테이너에서 관리하는 항목중에 일치하는것을 가져와서 알아서 사용함
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member name = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
